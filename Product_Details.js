4f840d017face29ec56dc75c360aaa9a61dacd54
divyayavthi <divyayavthii@gmail.com>
2025-08-20 07:52:48 +0530

    Update Product_Details.js

--- Product_Details.js
+++ Product_Details.js
@@ -6,83 +6,147 @@
 // this is arrow function so it is return the value and we can get from another then
 // then((json)) -->get json data so using this set the value for the setproduct
 import { useEffect, useState } from "react";
-import { Typography, TextField ,Button} from "@mui/material";
+import { Typography, TextField, Button } from "@mui/material";
 const ProductDetails = () => {
   // fetching data from api
   // product showing data to table
   const [products, setproducts] = useState([]);
-  const [newTitle,setNewTitle]=useState('')
+  const [newTitle, setNewTitle] = useState("");
+  const [newPrice, setNewPrice] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+  const [newCategory, setNewCategory] = useState("");
   //  data fetching from api end point
   //  data showed after ui component loaded
 
   useEffect(() => {
-    fetch(
-      "https://api.freeapi.app/api/v1/public/randomproducts?page=1&limit=10&inc=category%252Cprice%252Cthumbnail%252Cimages%252Ctitle%252Cid&query=mens-watches"
-    )
+    fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((result) => {
-        setproducts(result.data.data);
+        setproducts(result);
       });
   }, []);
+
+  function addProducts(){
+    // these are keys
+    const title =newTitle.trim();
+    const price =newPrice.trim();
+    const description =newDescription.trim();
+    const category =newCategory.trim();
+
+    if(title && price && description && category){
+      fetch('https://fakestoreapi.com/products',
+        // should be in object form
+        // should tell fetch function what method 
+        // 
+        {
+          method:"POST",
+          body:JSON.stringify({
+            title,price,description,category
+          }),
+          // server should understand what kind of data 
+          // ('Content-Type': 'application/json'; chartset=UTF-8 )
+          headers:{
+            'Content-Type': 'application/json' 
+          }
+              // we get the response in then function
+              // response may be in text format so we should convert it
+      }).then((response)=>response.json())
+      // we can get that Json data to this then via data variable
+      .then (data=>{
+        setproducts([...products,data])
+        console.log("data is added")
+      })
+    }
+  }
   // [] because of this empty array => useEffect calls only one time
   return (
     <div className="ProductDetails">
       <table>
         <thead>
-          <th>id</th>
-          <th>title</th>
-          <th>description</th>
-          <th>price</th>
-          <th>discountPercentage</th>
-          <th>rating</th>
-          <th>Action</th>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Action</th>
+          </tr>
         </thead>
         <tbody>
           {products.map((product) => (
             <tr key={product.id}>
               <td>{product.id}</td>
               <td>{product.title}</td>
-              <td>{product.description}</td>
-              <td>{product.price}</td>*59.0
-              <td>{product.discountPercentage}</td>
-              
+              <td>{product.price}</td>
               <td>
                 <TextField
                   label=""
                   multiline
-                  rows={1}
+                  rows={2}
                   variant="outlined"
-                  defaultValue={product.rating}
+                  defaultValue={product.description}
                 />
               </td>
-
+                <td>{product.category }</td>
               <td>
-                <Button variant="contained" color="black">
+                <Button variant="contained" color="primary">
                   Update
                 </Button>{" "}
               </td>
-              <Button variant="contained" color="black">
+              <td><Button variant="contained" color="red">
                 Delete
-              </Button>
+              </Button></td>
             </tr>
           ))}
         </tbody>
         <tfoot>
-            <tr>
-                <td><TextField
-  label="Tile"
-  placeholder="Enter Title"
-  variant="outlined"
-  value={newTitle}
-  onChange={(e)=>setNewTitle(e.target.value)}
-/></td>
-<td><Button variant="contained" color="black" onClick={addproduct}>
+          <tr>
+            <td>
+              <TextField
+                label="Tile"
+                placeholder="Enter Title"
+                variant="outlined"
+                value={newTitle}
+                onChange={(e) => setNewTitle(e.target.value)}
+              />
+            </td>
+            <td>
+              <TextField
+                label="Price"
+                placeholder="Enter Price"
+                variant="outlined"
+                value={newPrice}
+                onChange={(e) => setNewPrice(e.target.value)}
+              />
+            </td>
+            <td>
+              <TextField
+                label="Description"
+                placeholder="Enter Description"
+                variant="outlined"
+                value={newDescription}
+                onChange={(e) => setNewDescription(e.target.value)}
+              />
+            </td>
+             <td>
+              <TextField
+                label="Category"
+                placeholder="Enter Description"
+                variant="outlined"
+                value={newCategory}
+                onChange={(e) => setNewCategory(e.target.value)}
+              />
+            </td>
+            <td>
+              <Button variant="contained" color="black" onClick={addProducts}>
                 Add Product
-              </Button></td>
-            </tr>
+              </Button>
+            </td>
+          </tr>
         </tfoot>
       </table>
     </div>
   );
 };
 export default ProductDetails;
+
